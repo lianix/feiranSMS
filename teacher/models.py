6b2fd1c047c4eb6891b3ef1cd2002dd7956cec6e
LMH <lianix@163.com>
2020-01-31 13:45:21 +0800

    teacher: add teacher models
    Signed-off-by: LMH <lianix@163.com>

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from school.models import Human
+
+class Teacher(Human):
+    TYPE_ITEMS = [
+        (0, '临时教师'),
+        (1, '兼职教师'),
+        (2, '全职教师'),
+    ]
+    STATUS_ITEMS = [
+        (0, '待考核'),
+        (1, '正常'),
+        (2, '休假'),
+    ]
+
+    kind = models.IntegerField(choices=TYPE_ITEMS, default=0, verbose_name="类型")
+    status = models.IntegerField(choices=STATUS_ITEMS, default=0, verbose_name="状态")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = verbose_name_plural = "教师信息"
